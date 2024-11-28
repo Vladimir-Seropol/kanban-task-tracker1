08c4d4dea683bed996818218b70353654f9a6c47
Aleksandr G <inkowl94@gmail.com>
2024-11-28 20:19:06 +0300

    feat: added Inter fonts with variable

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,24 @@
-import Head from "next/head";
-import Image from "next/image";
-import localFont from "next/font/local";
-import styles from "@/styles/Home.module.css";
+import Head from 'next/head';
+import Image from 'next/image';
+import localFont from 'next/font/local';
+import styles from '@/styles/Home.module.css';
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter = localFont({
+  src: [
+    {
+      path: './fonts/Inter-Medium.ttf',
+      weight: '500',
+    },
+    {
+      path: './fonts/Inter-Regular.ttf',
+      weight: '400',
+    },
+    {
+      path: './fonts/Inter-SemiBold.ttf',
+      weight: '600',
+    },
+  ],
+  variable: '--font-inter',
 });
 
 export default function Home() {
@@ -23,9 +30,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div
-        className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
-      >
+      <div className={`${styles.page} ${inter.className}`}>
         <main className={styles.main}>
           <Image
             className={styles.logo}
