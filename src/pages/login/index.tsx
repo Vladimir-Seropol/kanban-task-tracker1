f8f602748f17be186c4245959ac0bfc569a332a6
DmitriyV <dmitrio87@mail.ru>
2024-12-01 19:14:03 +0300

    feat: добавил редакс тулкит и сделал пост запрос

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,10 +5,16 @@ import { useForm } from 'react-hook-form';
 import { LoginType } from '../../types/Login/Login';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import { useTokenApiMutation } from '../../redux/services/AuthApi';
+import { useAppDispatch } from '@/redux/hooks/hooks';
+import { useIsomorphicLayoutEffect } from 'swr/_internal';
+import { useEffect } from 'react';
+import { setUser } from '@/redux/features/auth/authSlice';
+
 const schema = yup
   .object()
   .shape({
-    mail: yup
+    email: yup
       .string()
       .email('Нужно заполнить')
       .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, 'Нужно заполнить')
@@ -17,6 +23,11 @@ const schema = yup
   })
   .required();
 export default function Login() {
+  const dispatch = useAppDispatch();
+  console.log(dispatch);
+  const [tokenApi, { data: tokenData, isSuccess: tokenSuccess }] =
+    useTokenApiMutation();
+  console.log(tokenApi);
   const {
     register,
     handleSubmit,
@@ -25,23 +36,30 @@ export default function Login() {
   } = useForm<LoginType>({ resolver: yupResolver(schema) });
   const onSubmit = (loginData: LoginType) => {
     console.log(`loginData`, loginData);
+
+    console.log(tokenApi(loginData));
     reset();
   };
+  useEffect(() => {
+    if (tokenSuccess) {
+      dispatch(setUser({ token: tokenData.token }));
+    }
+  }, [tokenSuccess]);
 
   return (
     <main className={`${style.login} ${inter.className}`}>
       <div className="container">
         <h2 className={style.title}>Вход</h2>
         <form className={style.form} onSubmit={handleSubmit(onSubmit)}>
-          <label htmlFor="mail">
+          <label htmlFor="email">
             <span>Электронная почта</span>
             <input
               type="email"
               placeholder="Электронная почта"
-              {...register('mail')}
+              {...register('email')}
             />
-            {errors.mail && (
-              <span className={style.errorText}>{errors.mail.message}</span>
+            {errors.email && (
+              <span className={style.errorText}>{errors.email.message}</span>
             )}
           </label>
           <label htmlFor="password">
---
+++ src/redux/features/auth/authSlice.tsx
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '@/redux/store';
+
+export interface AuthState {
+  token: string | null;
+}
+const initialState: AuthState = {
+  token: null,
+};
+const auth = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<{ token: string }>) => {
+      localStorage.setItem(
+        'token',
+        JSON.stringify({
+          token: action.payload.token,
+        }),
+      );
+
+      state.token = action.payload.token;
+    },
+  },
+});
+
+export const { setUser } = auth.actions;
+
+export default auth.reducer;
+
+export const selectCurrentUser = (state: RootState) => state.authApi;
