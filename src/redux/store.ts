19a262711029ff323e62941f44a501b3770a6056
Vladimir Seropol <frontender23@mail.ru>
2024-12-01 19:48:35 +0300

    Сохранить мои изменения

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-cycle */
+/* eslint-disable import/newline-after-import */
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { authApi } from './services/AuthApi';
@@ -7,7 +9,7 @@ export const store = configureStore({
     [authApi.reducerPath]: authApi.reducer,
     auth: authReducer,
   },
-  middleware: (getDefaultMiddleware) =>
+  middleware: (getDefaultMiddleware: () => string | any[]) =>
     getDefaultMiddleware().concat(authApi.middleware),
 });
 
