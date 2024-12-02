1dc5ed426c87684046b5b60be2a62123a2911313
Aleksandr G <inkowl94@gmail.com>
2024-12-02 13:52:38 +0300

    feat: included sidebar

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -3,22 +3,16 @@
 /* eslint-disable react/no-array-index-key */
 import { useState } from 'react';
 import Link from 'next/link';
-import Button from '@/components/Button';
 import TextInput from '@/components/Inputs/TextInput/TextInput';
 import CardInternal from '@/components/ProjectsCard/CardInternal';
 import CardDemo from '@/components/ProjectsCard/CardDemo';
-import Image from 'next/image';
+import Layout from '@/pages/projects/layout';
 import style from './style.module.css';
 
 export default function Board() {
-  const [isClicked, setIsClicked] = useState(false); // Состояние для отслеживания клика
   const [showArchived, setShowArchived] = useState(false); // Состояние для отображения архивных проектов
   const [archivedVisible, setArchivedVisible] = useState(false); // Для плавного отображения архивных проектов
   const [projectsVisible, setProjectsVisible] = useState(true); // Для плавного отображения всех проектов
-
-  const handleClick = () => {
-    setIsClicked(!isClicked); // Меняем состояние при клике
-  };
 
   const handleCheckboxChange = () => {
     setShowArchived((prev) => !prev); // Переключаем состояние чекбокса
@@ -27,137 +21,57 @@ export default function Board() {
   };
 
   return (
-    <main className="main">
-      <div className={style.board}>
-        <div
-          className={`${style.board__left} ${isClicked ? style.is_clicked : ''}`}
-        >
-          <div className={style.board__left_header}>
-            <Image
-              className={style.board__left_header_logo}
-              src="/logo_board.png"
-              alt="logo_board"
-              width={103} // Укажите ширину изображения
-              height={21}
-            />
-            <Image
-              className={style.board__left_header_icon}
-              src="/icon_board1.svg"
-              alt="icon_board"
-              width={24} // Укажите ширину изображения
-              height={24}
-              onClick={handleClick}
-              onKeyDown={(e: { key: string }) => {
-                if (e.key === 'Enter' || e.key === ' ') {
-                  handleClick();
-                }
-              }}
-              role="button"
-              tabIndex={0}
-              style={{ cursor: 'pointer' }}
+    <Layout>
+      <main className={style.board__right}>
+        <div className={style.board__right_header}>
+          <nav>
+            <Link href="/">
+              <span>Главная /</span>{' '}
+            </Link>
+            <span>Проекты</span>
+          </nav>
+        </div>
+        <div className={style.board__right_title}>
+          <h2>Проекты</h2>
+        </div>
+        <div className={style.board__right_selection}>
+          <div className={style.board__right_selection_item}>
+            <TextInput
+              label="Название проекта"
+              placeholder="Введите название проекта"
+              value={''}
+              onChange={() => {}}
             />
           </div>
-          <div className={style.board__left_user}>
-            <h2 style={{ color: '#fff' }}>User</h2>
-            <Button
-              text="Выйти"
-              inlineStyle={{
-                width: '100%',
-                height: '24px',
-                padding: '4.5px',
-                background: '#2D2D2D',
-                fontSize: '12px',
-                lineHeight: '14.52px',
-                color: '#787878',
-              }}
-              type="button"
-              onClick={() => {}}
+          <div className={style.board__right_selection_item}>
+            <TextInput
+              label="Номер задачи"
+              placeholder="Введите номер задачи"
+              value={''}
+              onChange={() => {}}
             />
-          </div>
-          <div className={style.board__left_project}>
-            <Image
-              src="icon_board3.svg"
-              alt="icon_board"
-              width={18} // Укажите ширину изображения
-              height={18}
-            />
-            <h4 style={{ color: '#fff' }}>Проекты</h4>
           </div>
         </div>
 
-        <div className={style.board__right}>
-          <div className={style.board__right_header}>
-            <nav>
-              <Link href="/">
-                <span>Главная /</span>{' '}
-              </Link>
-              <span>Проекты</span>
-            </nav>
-          </div>
-          <div className={style.board__right_title}>
-            <h2>Проекты</h2>
-          </div>
-          <div className={style.board__right_selection}>
-            <div className={style.board__right_selection_item}>
-              <TextInput
-                label="Название проекта"
-                placeholder="Введите название проекта"
-                value={''}
-                onChange={() => {}}
-              />
-            </div>
-            <div className={style.board__right_selection_item}>
-              <TextInput
-                label="Номер задачи"
-                placeholder="Введите номер задачи"
-                value={''}
-                onChange={() => {}}
-              />
-            </div>
-          </div>
+        <div className={style.board__right_selection_checkbox}>
+          <input
+            type="checkbox"
+            id="checkbox"
+            checked={showArchived}
+            onChange={handleCheckboxChange}
+          />
+          <label htmlFor="checkbox">Показать архивные проекты</label>
+        </div>
 
-          <div className={style.board__right_selection_checkbox}>
-            <input
-              type="checkbox"
-              id="checkbox"
-              checked={showArchived}
-              onChange={handleCheckboxChange}
-            />
-            <label htmlFor="checkbox">Показать архивные проекты</label>
-          </div>
-
-          <div className={style.board__right_projects}>
-            <div
-              className={`${style.board__right_archived_projects} ${archivedVisible ? style.show : ''}`}
-            >
-              {showArchived && (
-                <>
-                  <h5 style={{ marginBottom: '16px' }}>Архивные проекты</h5>
-                  <div className={style.board__right_selected_projects}>
-                    {Array.from({ length: 4 }).map((_, index) => (
-                      <div
-                        key={index}
-                        className={style.board__right_selected_internal_item}
-                      >
-                        <CardInternal />
-                      </div>
-                    ))}
-                  </div>
-                </>
-              )}
-            </div>
-
-            {!showArchived && (
-              <div
-                className={`${style.board__right_featured} ${projectsVisible ? style.show : ''}`}
-              >
-                <h5 style={{ marginBottom: '16px' }}>Избранные проекты</h5>
+        <div className={style.board__right_projects}>
+          <div
+            className={`${style.board__right_archived_projects} ${archivedVisible ? style.show : ''}`}
+          >
+            {showArchived && (
+              <>
+                <h5 style={{ marginBottom: '16px' }}>Архивные проекты</h5>
                 <div className={style.board__right_selected_projects}>
-                  <CardInternal />
-                  <CardDemo />
-                </div>
-                <div className={style.board__right_internal_projects}>
-                  {Array.from({ length: 16 }).map((_, index) => (
+                  {Array.from({ length: 4 }).map((_, index) => (
                     <div
                       key={index}
                       className={style.board__right_selected_internal_item}
@@ -166,11 +80,33 @@ export default function Board() {
                     </div>
                   ))}
                 </div>
-              </div>
+              </>
             )}
           </div>
+
+          {!showArchived && (
+            <div
+              className={`${style.board__right_featured} ${projectsVisible ? style.show : ''}`}
+            >
+              <h5 style={{ marginBottom: '16px' }}>Избранные проекты</h5>
+              <div className={style.board__right_selected_projects}>
+                <CardInternal />
+                <CardDemo />
+              </div>
+              <div className={style.board__right_internal_projects}>
+                {Array.from({ length: 16 }).map((_, index) => (
+                  <div
+                    key={index}
+                    className={style.board__right_selected_internal_item}
+                  >
+                    <CardInternal />
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
         </div>
-      </div>
-    </main>
+      </main>
+    </Layout>
   );
 }
--- src/pages/projects/slug/index.tsx
+++ src/pages/projects/slug/index.tsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 import CustomDatePicker from '@/components/Inputs/DatePicker/CustomDatePicker';
 import SelectInput from '@/components/Inputs/SelectInput/SelectInput';
 import TextInput from '@/components/Inputs/TextInput/TextInput';
+import Layout from '@/pages/projects/layout';
 import style from './style.module.css';
 
 interface User {
@@ -16,7 +17,6 @@ interface User {
 export default function Slug() {
   // Состояние для хранения данных о пользователе
   const [user, setUser] = useState(null);
-  const [isClicked, setIsClicked] = useState(false); // Состояние для отслеживания клика
   const [selectedUsers, setSelectedUsers] = useState<User[]>([]);
 
   // Добавляем состояние для выбранной даты
@@ -45,167 +45,107 @@ export default function Slug() {
     { id: 3, firstName: 'Сергей', lastName: 'Сергеев' },
   ];
 
-  const handleClick = () => {
-    setIsClicked(!isClicked); // Меняем состояние при клике
-  };
-
   return (
-    <main className="main">
-      <div className={style.board}>
-        <div
-          className={`${style.board__left} ${isClicked ? style.is_clicked : ''}`}
-        >
-          <div className={style.board__left_header}>
-            <Image
-              className={style.board__left_header_logo}
-              src="/logo_board.png"
-              alt="logo_board"
-            />
-            <Image
-              className={style.board__left_header_icon}
-              src="/icon_board1.svg"
-              alt="icon_board"
-              onClick={handleClick}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter' || e.key === ' ') {
-                  handleClick();
-                }
-              }}
-              role="button"
-              tabIndex={0}
-              style={{ cursor: 'pointer' }}
-            />
-          </div>
-          <div className={style.board__left_user}>
-            <h2 style={{ color: '#fff' }}>{user?.name}</h2>
-            <Button
-              text="Выйти"
-              inlineStyle={{
-                width: '100%',
-                height: '24px',
-                padding: '4.5px',
-                background: '#2D2D2D',
-                fontSize: '12px',
-                lineHeight: '14.52px',
-                color: '#787878',
-              }}
-              type="button"
-              // eslint-disable-next-line no-console
-              onClick={() => console.log('click')}
-            />
-          </div>
-          <div className={style.board__left_project}>
-            <Link href="/projects">
-              <Image
-                src="/icon_board3.svg"
-                alt="icon_board"
-                style={{ verticalAlign: 'top', marginRight: '8px' }}
-              />
-              <h4 style={{ color: '#fff', display: 'inline-block' }}>
-                Проекты
-              </h4>
+    <Layout>
+      <main className={style.board__right}>
+        <div className={style.board__right_header}>
+          <nav>
+            <Link href="/">
+              <span>Главная / </span>
             </Link>
-          </div>
+            <Link href="/projects">
+              <span>Проекты / </span>
+            </Link>
+            <span>Demo Project</span>
+          </nav>
         </div>
+        <div className={style.board__right_title}>
+          <div className={style.board__right_title_checkbox}>
+            <h2>Demo Project</h2>
+            <input type="checkbox" />
+          </div>
 
-        <div className={style.board__right}>
-          <div className={style.board__right_header}>
-            <nav>
-              <Link href="/">
-                <span>Главная / </span>
-              </Link>
-              <Link href="/projects">
-                <span>Проекты / </span>
-              </Link>
-              <span>Demo Project</span>
-            </nav>
-          </div>
-          <div className={style.board__right_title}>
-            <div className={style.board__right_title_checkbox}>
-              <h2>Demo Project</h2>
-              <input type="checkbox" />
-            </div>
-
-            {/* Условное отображение кнопки "Добавить задачу" */}
-            {user?.is_admin && (
-              <Button
-                svg={
-                  <Image
-                    src="/icon_create.svg"
-                    alt="icon_create"
-                    style={{ verticalAlign: 'middle', marginRight: '8px' }}
-                  />
-                }
-                text="Добавить задачу"
-                type="button"
-                onClick={() => {}}
-              />
-            )}
-          </div>
-          <div className={style.board__right_selection}>
-            <div className={style.board__right_selection_item}>
-              <TextInput
-                placeholder="Название проекта"
-                value=""
-                onChange={() => {}}
-                label="Название проекта"
-              />
-            </div>
-            <div className={style.board__right_selection_item}>
-              <SelectInput
-                label="Выбрать пользователей"
-                options={users}
-                value={selectedUsers}
-                onChange={setSelectedUsers}
-              />
-            </div>
-            <div className={style.board__right_selection_item}>
-              <SelectInput
-                label="Выбрать тип"
-                options={users}
-                value={selectedUsers}
-                onChange={setSelectedUsers}
-              />
-            </div>
-            <div className={style.board__right_selection_item}>
-              <SelectInput
-                label="Выбрать компонент"
-                options={users}
-                value={selectedUsers}
-                onChange={setSelectedUsers}
-              />
-            </div>
-          </div>
-          <div className={style.board__right_date}>
-            <CustomDatePicker
-              placeholder="Дата начала"
-              value={startDate}
-              onChange={setStartDate}
-              className={style.my_custom_class}
-              inputClassName={style.my_input_class}
+          {/* Условное отображение кнопки "Добавить задачу" */}
+          {user?.is_admin && (
+            <Button
+              svg={
+                <Image
+                  src="/icon_create.svg"
+                  alt="icon_create"
+                  style={{ verticalAlign: 'middle', marginRight: '8px' }}
+                  width={24}
+                  height={24}
+                />
+              }
+              text="Добавить задачу"
+              type="button"
+              onClick={() => {}}
             />
-
-            <CustomDatePicker
-              placeholder="Дата завершения"
-              value={endDate}
-              onChange={setEndDate}
-              className={style.my_custom_class}
-              inputClassName={style.my_input_class}
+          )}
+        </div>
+        <div className={style.board__right_selection}>
+          <div className={style.board__right_selection_item}>
+            <TextInput
+              placeholder="Название проекта"
+              value=""
+              onChange={() => {}}
+              label="Название проекта"
             />
           </div>
-          <div className={style.board__right_tasks}>
-            <div className={style.board__right_tasks_item}>Новые</div>
-            <div className={style.board__right_tasks_item}>В работе</div>
-            <div className={style.board__right_tasks_item}>Выполнено</div>
-            <div className={style.board__right_tasks_item}>В ревью</div>
-            <div className={style.board__right_tasks_item}>
-              Готовы к тестированию
-            </div>
-            <div className={style.board__right_tasks_item}>В тестировании</div>
-            <div className={style.board__right_tasks_item}>Решены</div>
+          <div className={style.board__right_selection_item}>
+            <SelectInput
+              label="Выбрать пользователей"
+              options={users}
+              value={selectedUsers}
+              onChange={setSelectedUsers}
+            />
+          </div>
+          <div className={style.board__right_selection_item}>
+            <SelectInput
+              label="Выбрать тип"
+              options={users}
+              value={selectedUsers}
+              onChange={setSelectedUsers}
+            />
+          </div>
+          <div className={style.board__right_selection_item}>
+            <SelectInput
+              label="Выбрать компонент"
+              options={users}
+              value={selectedUsers}
+              onChange={setSelectedUsers}
+            />
           </div>
         </div>
-      </div>
-    </main>
+        <div className={style.board__right_date}>
+          <CustomDatePicker
+            placeholder="Дата начала"
+            value={startDate}
+            onChange={setStartDate}
+            className={style.my_custom_class}
+            inputClassName={style.my_input_class}
+          />
+
+          <CustomDatePicker
+            placeholder="Дата завершения"
+            value={endDate}
+            onChange={setEndDate}
+            className={style.my_custom_class}
+            inputClassName={style.my_input_class}
+          />
+        </div>
+        <div className={style.board__right_tasks}>
+          <div className={style.board__right_tasks_item}>Новые</div>
+          <div className={style.board__right_tasks_item}>В работе</div>
+          <div className={style.board__right_tasks_item}>Выполнено</div>
+          <div className={style.board__right_tasks_item}>В ревью</div>
+          <div className={style.board__right_tasks_item}>
+            Готовы к тестированию
+          </div>
+          <div className={style.board__right_tasks_item}>В тестировании</div>
+          <div className={style.board__right_tasks_item}>Решены</div>
+        </div>
+      </main>
+    </Layout>
   );
 }
