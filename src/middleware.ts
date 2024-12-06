3e7a7692d2d69711796d72e9d51af7ff3fefaa53
Aleksandr G <inkowl94@gmail.com>
2024-12-06 13:07:50 +0300

    fix: logout in header

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,43 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
+  const { pathname }: { pathname: string } = request.nextUrl;
   const authToken = request.cookies.get('auth_token');
 
-  if (pathname.startsWith('/_next') || pathname.startsWith('/static')) {
+  // Пропуск статических файлов, включая изображения в папке public
+  if (
+    pathname.startsWith('/_next') ||
+    pathname.startsWith('/static') ||
+    pathname.startsWith('/Logo.png')
+  ) {
+    return NextResponse.next();
+  }
+
+  // Главная страница доступна всем
+  if (pathname === '/') {
     return NextResponse.next();
   }
 
   // Если токена нет и мы не на странице входа, редиректим на вход
-  if ((!authToken || !authToken?.value) && pathname !== '/login') {
-    request.cookies.delete('auth_token');
+  if (!authToken && pathname !== '/login') {
     return NextResponse.redirect(new URL('/login', request.url));
   }
 
   // Если токен есть и мы на странице входа, редиректим на главную
-  if (authToken && authToken?.value && pathname === '/login') {
+  if (authToken && pathname === '/login') {
     return NextResponse.redirect(new URL('/', request.url));
   }
 
-  // По идее это должно было работать :)
   // Если токен есть, добавляем его в заголовок
-  // if (authToken && authToken?.value) {
-  //   request.headers.set('authorization', `Bearer ${authToken.value}`);
-  //
-  //   return NextResponse.next();
-  // }
+  if (authToken) {
+    const response = NextResponse.next();
+    response.headers.set('authorization', `Bearer ${authToken}`);
+    return response;
+  }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/:path*'],
+  matcher: ['/:path((?!^$).*)'], // Применяем middleware ко всем маршрутам, кроме корневого
 };
