b795bac8588fbf412207fb278cc490f017214726
Aleksandr G <inkowl94@gmail.com>
2024-12-06 19:19:45 +0300

    refactor: UserInfo component

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Image from 'next/image';
-import profilePic from '../../assets/images/user/Ellipse 27.jpg';
-import style from './UserAuthIdComponent.module.css';
-import { UserTypeResponse } from '@/types/UserTypeResponse';
+import profilePic from '@/assets/images/user/Ellipse 27.jpg';
+import style from './UserInfo.module.css';
 
-export default function UserInfo(user: UserTypeResponse) {
-  const { name, position } = user;
+export default function UserInfo({
+  name,
+  position,
+}: {
+  name: string;
+  position: string;
+}) {
   return (
     <div className={style.wrapper_UserAuth}>
       <Image
